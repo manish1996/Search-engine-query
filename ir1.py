e0e6db857be1d7b4326bf4482ee8be0754c58cd2
manish1996 <singla.iiitv@gmail.com>
2017-02-04 15:49:48 +0530

    first initial code to read the terms

---
+++ ir1.py
@@ -0,0 +1,24 @@
+import sys
+import xml.etree.ElementTree as ET # etree library for xml data parsing
+f4 = open("wordCount.txt", 'r+')  #output file
+output=''
+allfiles=sys.argv  # commandline input all text files
+fileposition=1  # pointer to traverse through all the txt files
+
+while fileposition<len(allfiles):
+	tree = ET.parse(allfiles[fileposition])  #read all the document files
+	root = tree.getroot()
+	for j in root.findall('title'): # find title field
+		out1 = j.text
+	for k in root.findall('content'): # find content field
+		out2 = k.text
+	output+= out1 + out2  #string hold all the texts
+	fileposition+=1  # increment pointer to access next file
+ 
+print(output) #content of all the files
+m= output.split(" ")
+n = set(m)
+w = list(n) #list of all unique words
+for i in range(len(w)):
+	f4.write(str(w[i] + '\t\t'))  # write each word in wordCount.txt
+	f4.write(str(m.count(str(w[i]))) + '\n') # write number of occurence of each word
